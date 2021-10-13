b7a9179f8dc8a311a3239dfe8205963b089885bf
Nam Nguyen <namnguyen0905@outlook.com>
2021-10-13 19:32:49 -0400

    Add feature to auto-close tab on authorization; Keep user logged in when navigating to other tabs

--- dist/content-script.js
+++ dist/content-script.js
@@ -1,3 +1,7 @@
-const params = new URLSearchParams(window.location.search);
+if (window.location.href.indexOf("localhost:3000/callback") > -1) {
+	const params = new URLSearchParams(window.location.search);
 
-chrome.storage.local.set({ token: params.get('token') });
+	chrome.storage.local.set({ token: params.get('token') });
+
+	window.close();
+}
